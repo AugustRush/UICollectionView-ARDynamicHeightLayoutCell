d2755047d5e33c5ebbc8c54a2a82074be64fa250
AugustRush <liupingwei30@gmail.com>
2016-01-05 15:20:19 +0800

    dispatch_once for swizzling methods

--- UICollectionView+ARDynamicHeightLayoutCell/UICollectionView+ARDynamicCacheHeightLayoutCell.h
+++ UICollectionView+ARDynamicHeightLayoutCell/UICollectionView+ARDynamicCacheHeightLayoutCell.h
@@ -1,24 +1,25 @@
-//The MIT License (MIT)
+// The MIT License (MIT)
 //
-//Copyright (c) 2015
+// Copyright (c) 2015
 //
-//Permission is hereby granted, free of charge, to any person obtaining a copy
-//of this software and associated documentation files (the "Software"), to deal
-//in the Software without restriction, including without limitation the rights
-//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//copies of the Software, and to permit persons to whom the Software is
-//furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//The above copyright notice and this permission notice shall be included in all
-//copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all
+// copies or substantial portions of the Software.
 //
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//SOFTWARE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #import <UIKit/UIKit.h>
 
@@ -33,9 +34,11 @@
  *
  *  @return the caculated cell's height
  */
--(CGSize)ar_sizeForCellWithIdentifier:(NSString *)identifier
-                            indexPath:(NSIndexPath *)indexPath
-                        configuration:(void(^)(__kindof UICollectionViewCell *))configuration;
+- (CGSize)ar_sizeForCellWithIdentifier:(NSString *)identifier
+                             indexPath:(NSIndexPath *)indexPath
+                         configuration:
+                             (void (^)(__kindof UICollectionViewCell *))
+                                 configuration;
 
 /**
  *  caculate cell size with a fixed width
@@ -48,10 +51,12 @@
  *  @return the caculated cell's height
  */
 
--(CGSize)ar_sizeForCellWithIdentifier:(NSString *)identifier
-                            indexPath:(NSIndexPath *)indexPath
-                           fixedWidth:(CGFloat)fixedWidth
-                        configuration:(void (^)(__kindof UICollectionViewCell * cell))configuration;
+- (CGSize)ar_sizeForCellWithIdentifier:(NSString *)identifier
+                             indexPath:(NSIndexPath *)indexPath
+                            fixedWidth:(CGFloat)fixedWidth
+                         configuration:
+                             (void (^)(__kindof UICollectionViewCell *cell))
+                                 configuration;
 
 /**
  *  caculate cell size with fixed height
@@ -64,9 +69,11 @@
  *  @return the caculated cell's height
  */
 
--(CGSize)ar_sizeForCellWithIdentifier:(NSString *)identifier
-                            indexPath:(NSIndexPath *)indexPath
-                          fixedHeight:(CGFloat)fixedHeight
-                        configuration:(void (^)(__kindof UICollectionViewCell * cell))configuration;
+- (CGSize)ar_sizeForCellWithIdentifier:(NSString *)identifier
+                             indexPath:(NSIndexPath *)indexPath
+                           fixedHeight:(CGFloat)fixedHeight
+                         configuration:
+                             (void (^)(__kindof UICollectionViewCell *cell))
+                                 configuration;
 
 @end
